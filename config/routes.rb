4815b6f7aca3bab8898e8c802b8ded7b85cef9ec
Sileo1024 <natalia.niziolek@gmail.com>
2017-04-17 19:48:25 +0200

    added empty home page and roote

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-	root to: redirect('challange_walls')
-  resources :challange_walls
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+devise_for :users
+root 'home#index'
+
+resources :challange_walls
+
 end

f932d91ec521fd362fe92774c0ccadcf6cddf58d
Sileo1024 <natalia.niziolek@gmail.com>
2017-04-17 15:14:38 +0200

    added ChallangeWalls to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411132327) do
+ActiveRecord::Schema.define(version: 20170417131123) do
+
+  create_table "challange_walls", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
